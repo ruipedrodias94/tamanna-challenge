58f0b5c09bd15d797ac333d6eff6b2cdb2137f28
Rui Pedro Dias <dias94.rui.pedro@gmail.com>
2022-07-05 03:06:41 +0100

    feat: request for open weather api for location

---
+++ weather-app/pages/api/getWeather.ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  let urlRequest = "";
+
+  if (req.method === "GET") {
+    if (req.query.lat && req.query.long) {
+      urlRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${40.3396822}&lon=${-8.4254256}&appid=${
+        process.env.OWAK
+      }&units=metric`;
+    } else if (req.query.city) {
+      urlRequest = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city},${req.query.country}&appid=${process.env.OWAK}&units=metric`;
+    }
+
+    try {
+      const response = await axios.get(urlRequest);
+
+      if (response.status === 200) {
+        res.status(200).json(response.data);
+      }
+    } catch (error: any) {
+      throw new Error("message error: ", error);
+    }
+  }
+}
