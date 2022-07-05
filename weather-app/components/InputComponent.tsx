d4b2101c06f58fce4933ba51684878fec9f98199
Rui Pedro Dias <dias94.rui.pedro@gmail.com>
2022-07-05 03:05:06 +0100

    fix: deleted unwanted files

--- weather-app/components/InputComponent.tsx
+++
@@ -1,9 +0,0 @@
-export default function InputComponent() {
-  return (
-    <div>
-      <h1>Hey there from input InputComponent</h1>
-      Insert another city
-      <input placeholder="text here" />
-    </div>
-  );
-}
